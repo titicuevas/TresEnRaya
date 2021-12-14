42c82c102539b37d0afeec060a42984a246ae794
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2021-12-14 14:43:33 +0100

    Pinto la tabla y los jugadores

--- index.py
+++ index.py
@@ -1,48 +1,66 @@
-tablero=(" " * 9) #Nuestras casillas creadas estan vacias
-#Colocamos una ficha en el tablero
-def coordenada(literal,inferior,superior):
+tablero= []  # Nuestras casillas creadas estan vacias
+
+# Inicializamos el tablero
+for i in range(9):
+    tablero.append(' ')
+
+
+# Colocamos una ficha en el tablero
+def coordenada(literal, inferior, superior):
     while True:
-     Valor=input(literal)
-     while(not Valor.isnumeric()):
-         print("Solo se admiten numeros entre {0} y {1}".format(inferior,superior))
-         valor=input(literal)
-     coor=int(valor)
-     if (coor>inferior and superior <=superior):
-         return coor
-     
+        valor = input(literal)
+        while (not vSalor.isnumeric()):
+            print("Solo se admiten numeros entre {0} y {1}".format(inferior, superior))
+            valor = input(literal)
+        coor = int(valor)
+        if (coor>=inferior and coor<=superior):
+            return coor
+
     else:
-        print("El valor indicado es incorrecto,introduzca un numero entre {0} y {1}".format(inferior,superior))
-    
-    
+        print(
+            "El valor indicado es incorrecto,introduzca un numero entre {0} y {1}".format(inferior, superior))
+
+
 def colocarFicha(ficha):
     print("Dame la posicion de una ficha")
     while True:
-        fila=coordenada("Fila: ")
-        columna=coordenada("Columna: ")
-        
-        #Es un tablero 3x3
-        casilla=fila*3+columna
-        if(tablero[casilla]!=' '):
-            #Casilla cubierta
+        fila =coordenada("Fila entre [1-3]: ", 1, 3) - 1  # Restamos 1 para que sea de 0-2
+        columna =coordenada("Columna entre [1-3]: ", 1, 3) - 1  # Restamos 1 para que sea de 0-2
+
+        # Es un tablero 3x3
+        casilla = fila * 3 + columna
+        if (tablero[casilla] !=' '):
+            # Casilla cubierta
             print("La casilla esta ocupada")
         else:
-            tablero[casilla]=ficha
-            return
-        
-
-jugador1=input("Introduzca el nombre del jugador 1: ")
-jugador2=input("Introduzca el nombre del jugador 2: ")
+            tablero[casilla] = ficha
+            return 
 
 
-#Inicio el juego
+def pintarTablero():
+    pos: 0
+    print("-" * 18)
+    for fila in range(3):
+        for columna in range(3):
+            print("| " ,tablero[pos], " ", end="")
+            pos += 1
+        print("|\n",("-"*18))
 
-continuar=True
-fichasEnTablero=0
+
+jugador1 = input("Introduzca el nombre del jugador 1: ")
+jugador2 = input("Introduzca el nombre del jugador 2: ")
+
+
+# Inicio el juego
+
+continuar = True
+fichasEnTablero = 0
 while continuar:
-    #Pedimos posicion fichas
-    
-    colocarFicha('X'if (fichasEnTablero&1)else 'O')
-    fichasEnTablero+=1
-    if(fichasEnTablero==9):
-        continuar=False
-    
+
+    # Pedimos posicion fichas
+    pintarTablero()
+    colocarFicha("X" if (fichasEnTablero & 1) else "O")
+   
+    fichasEnTablero += 1
+    if fichasEnTablero == 9:
+        continuar = False
