3d506fc22c3ac482c179a49013a6f839d1ce8b19
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2021-12-14 14:53:41 +0100

    Funciona para dos jugadores perfectamente

--- index.py
+++ index.py
@@ -1,50 +1,55 @@
-tablero= []  # Nuestras casillas creadas estan vacias
+tablero = []  # Nuestras casillas creadas estan vacias
 
 # Inicializamos el tablero
 for i in range(9):
-    tablero.append(' ')
+    tablero.append(" ")
 
 
 # Colocamos una ficha en el tablero
 def coordenada(literal, inferior, superior):
     while True:
         valor = input(literal)
-        while (not vSalor.isnumeric()):
+        while (not valor.isnumeric()):
             print("Solo se admiten numeros entre {0} y {1}".format(inferior, superior))
             valor = input(literal)
         coor = int(valor)
-        if (coor>=inferior and coor<=superior):
+        if coor >= inferior and coor <= superior:
             return coor
 
     else:
         print(
-            "El valor indicado es incorrecto,introduzca un numero entre {0} y {1}".format(inferior, superior))
+            "El valor indicado es incorrecto,introduzca un numero entre {0} y {1}".format(
+                inferior, superior
+            )
+        )
 
 
 def colocarFicha(ficha):
     print("Dame la posicion de una ficha")
     while True:
-        fila =coordenada("Fila entre [1-3]: ", 1, 3) - 1  # Restamos 1 para que sea de 0-2
-        columna =coordenada("Columna entre [1-3]: ", 1, 3) - 1  # Restamos 1 para que sea de 0-2
+        fila = coordenada("Fila entre [1-3]: ", 1, 3) - 1
+          # Restamos 1 para que sea de 0-2
+        columna = coordenada("Columna entre [1-3]: ", 1, 3) - 1
+          # Restamos 1 para que sea de 0-2
 
         # Es un tablero 3x3
         casilla = fila * 3 + columna
-        if (tablero[casilla] !=' '):
+        if tablero[casilla] != " ":
             # Casilla cubierta
             print("La casilla esta ocupada")
         else:
             tablero[casilla] = ficha
-            return 
+            return
 
 
 def pintarTablero():
-    pos: 0
+    pos=0
     print("-" * 18)
     for fila in range(3):
         for columna in range(3):
-            print("| " ,tablero[pos], " ", end="")
-            pos += 1
-        print("|\n",("-"*18))
+            print("| ", tablero[pos], " ", end="")
+            pos+= 1
+        print("|\n", ("-" * 18))
 
 
 jugador1 = input("Introduzca el nombre del jugador 1: ")
@@ -60,7 +65,7 @@ while continuar:
     # Pedimos posicion fichas
     pintarTablero()
     colocarFicha("X" if (fichasEnTablero & 1) else "O")
-   
+
     fichasEnTablero += 1
     if fichasEnTablero == 9:
         continuar = False
