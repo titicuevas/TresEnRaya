8e405674bca64cb3e55888a6e359e216c526ab10
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2021-12-14 13:13:54 +0100

    Update index.py
    Coloco las fichas en el tablero

--- index.py
+++ index.py
@@ -1,2 +1,16 @@
 jugador1=input("Introduzca el nombre del jugador 1: ")
 jugador2=input("Introduzca el nombre del jugador 2: ")
+
+
+#Inicio el juego
+continuar=True
+fichasEnTablero=0
+while continuar:
+    #Pedimos posicion fichas
+    input("Dame la posicion de una ficha")
+    fila=input("Fila: ")
+    columna=input("Columna: ")
+    fichasEnTablero+=1
+    if(fichasEnTablero==9):
+        continuar=False
+    
